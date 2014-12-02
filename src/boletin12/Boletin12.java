621c71d75480c183f4b8375260c2571e7d5cea07
ddizoya <ddizoya@danielcastelao.org>
2014-12-02 14:57:44 +0100

    casi acabado

--- src/boletin12/Boletin12.java
+++ src/boletin12/Boletin12.java
@@ -18,28 +18,65 @@ public class Boletin12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Garaxe gx = new Garaxe();
-        
-        
+        Garaxe gx1 = new Garaxe();
+        Garaxe gx2 = new Garaxe();
+        Garaxe gx3 = new Garaxe();
+        Garaxe gx4 = new Garaxe();
+        Garaxe gx5 = new Garaxe();
+        Garaxe aux = new Garaxe();
         int menu = Integer.parseInt(JOptionPane.showInputDialog(null, " ***MENU***\nSe quere deixar o seu coche no garaxe, prema 1."
                 + "\n Se quere retirar o seu coche, prema 2."));
 
         switch (menu) {
-    
+
             case 1:
-                gx.comprobarPlazas();    
-            
+                if (gx1.getNumeroCoches()<5)
+                gx1.comprobarPlazas();
+                gx2.comprobarPlazas();
+                gx3.comprobarPlazas();
+                gx4.comprobarPlazas();
+                gx5.comprobarPlazas();
+                break;
+
             case 2:
                 String ma = JOptionPane.showInputDialog("Introduce o número de matrícula do seu coche");
                 Date fecha = new Date();
-                if (ma.equals(gx.getMatricula1())){
-                    gx.setTf1(fecha.getTime());
-                    doublegx.getTf1()-gx.getTi1();
-                } else if (ma.equals(gx.getMatricula2())){
+                if (ma.equals(gx1.getMatricula())) {
+                    gx1.obterTf();
+                    gx1.calcularPrezo();
+                    gx1.pagar();
+                    gx1.factura();
+                    gx1.devolverNulos();
                     
-                }
+                } else if (ma.equals(gx2.getMatricula())) {
+                    gx2.obterTf();
+                    gx2.calcularPrezo();
+                    gx2.pagar();
+                    gx2.factura();
+                    gx2.devolverNulos();
+                } else if (ma.equals(gx3.getMatricula())){
+                    gx3.obterTf();
+                    gx3.calcularPrezo();
+                    gx3.pagar();
+                    gx3.factura();
+                    gx3.devolverNulos();
+                } else if (ma.equals(gx4.getMatricula())){
+                    gx4.obterTf();
+                    gx4.calcularPrezo();
+                    gx4.pagar();
+                    gx4.factura();
+                    gx4.devolverNulos();
+                } else{
+                    gx5.obterTf();
+                    gx5.calcularPrezo();
+                    gx5.pagar();
+                    gx5.factura();
+                    gx5.devolverNulos();
+                } break;
+            
             default:
-                JOptionPane.showConfirmDialog(null, "Cometeu un erro.");
+                JOptionPane.showConfirmDialog(null, "Cometeu un erro. Inténteo de novo ou máis tarde. Disculpe as molestias.");
+                
 
         }
 
--- src/boletin12/Garaxe.java
+++ src/boletin12/Garaxe.java
@@ -14,30 +14,57 @@ import java.util.Date;
  */
 public class Garaxe {
 
-    private int numeroCoches;
-    private String matricula1;
-    private String matricula2;
-    private String matricula3;
-    private String matricula4;
-    private String matricula5;
+    private static int numeroCoches;
+    private String matricula;
     private static double PREZOINICIAL = 0.40;
-    private long Ti1;
-    private long Tf1;
-    private long Ti2;
-    private long Tf2;
-    private long Ti3;
-    private long Tf3;
-    private long Ti4;
-    private long Tf4;
-    private long Ti5;
-    private long Tf5;
+    private double tiempo;
+    private double ti;
+    private double tf;
+    private double prezo;
+    private double prezodev;
+    private double prezorec;
+
+    public Garaxe(){
+        
+    }
+
+    public double getPrezo() {
+        return prezo;
+    }
+
+    public void setPrezo(double prezo) {
+        this.prezo = prezo;
+    }
+    public double getTi() {
+        return ti;
+    }
+
+    public void setTi(double ti) {
+        this.ti = ti;
+    }
+
+    public double getTf() {
+        return tf;
+    }
+
+    public void setTf(double tf) {
+        this.tf = tf;
+    }
+
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
 
     public int getNumeroCoches() {
         return numeroCoches;
     }
 
     public void setNumeroCoches(int numeroCoches) {
-        this.numeroCoches = numeroCoches;
+        Garaxe.numeroCoches = numeroCoches;
     }
 
     public String getMatricula() {
@@ -48,46 +75,6 @@ public class Garaxe {
         this.matricula = matricula;
     }
 
-    public String getMatricula1() {
-        return matricula1;
-    }
-
-    public void setMatricula1(String matricula1) {
-        this.matricula1 = matricula1;
-    }
-
-    public String getMatricula2() {
-        return matricula2;
-    }
-
-    public void setMatricula2(String matricula2) {
-        this.matricula2 = matricula2;
-    }
-
-    public String getMatricula3() {
-        return matricula3;
-    }
-
-    public void setMatricula3(String matricula3) {
-        this.matricula3 = matricula3;
-    }
-
-    public String getMatricula4() {
-        return matricula4;
-    }
-
-    public void setMatricula4(String matricula4) {
-        this.matricula4 = matricula4;
-    }
-
-    public String getMatricula5() {
-        return matricula5;
-    }
-
-    public void setMatricula5(String matricula5) {
-        this.matricula5 = matricula5;
-    }
-
     public static double getPREZOINICIAL() {
         return PREZOINICIAL;
     }
@@ -96,117 +83,52 @@ public class Garaxe {
         Garaxe.PREZOINICIAL = PREZOINICIAL;
     }
 
-    public long getTi1() {
-        return Ti1;
-    }
-
-    public void setTi1(long Ti1) {
-        this.Ti1 = Ti1;
-    }
-
-    public long getTf1() {
-        return Tf1;
-    }
-
-    public void setTf1(long Tf1) {
-        this.Tf1 = Tf1;
-    }
-
-    public long getTi2() {
-        return Ti2;
-    }
-
-    public void setTi2(long Ti2) {
-        this.Ti2 = Ti2;
-    }
-
-    public long getTf2() {
-        return Tf2;
-    }
-
-    public void setTf2(long Tf2) {
-        this.Tf2 = Tf2;
-    }
-
-    public long getTi3() {
-        return Ti3;
-    }
-
-    public void setTi3(long Ti3) {
-        this.Ti3 = Ti3;
-    }
-
-    public long getTf3() {
-        return Tf3;
-    }
-
-    public void setTf3(long Tf3) {
-        this.Tf3 = Tf3;
-    }
-
-    public long getTi4() {
-        return Ti4;
-    }
-
-    public void setTi4(long Ti4) {
-        this.Ti4 = Ti4;
-    }
-
-    public long getTf4() {
-        return Tf4;
-    }
-
-    public void setTf4(long Tf4) {
-        this.Tf4 = Tf4;
-    }
-
-    public long getTi5() {
-        return Ti5;
-    }
-
-    public void setTi5(long Ti5) {
-        this.Ti5 = Ti5;
-    }
-
-    public long getTf5() {
-        return Tf5;
-    }
-
-    public void setTf5(long Tf5) {
-        this.Tf5 = Tf5;
-    }
-
     public void comprobarPlazas() {
-        if (numeroCoches <= 5) {
+        if (numeroCoches >= 5) {
             JOptionPane.showMessageDialog(null, "COMPLETO");
         } else {
             JOptionPane.showMessageDialog(null, "PLAZAS DISPOÑIBLES:" + (5 - numeroCoches));
-            String mat;
+            String mat;            
             mat = JOptionPane.showInputDialog("Introduza a matrícula do seu coche:");
-            Date fecha = new Date();
-            if (matricula1 == null) {
-                this.matricula1 = mat;
-                this.Ti1 = fecha.getTime();
-            } else if (matricula2 == null) {
-                this.matricula2 = mat;
-                this.Ti2 = fecha.getTime();
-            } else if (matricula3 == null) {
-                this.matricula3 = mat;
-                this.Ti3 = fecha.getTime();
-            } else if (matricula4 == null) {
-                this.matricula4 = mat;
-                this.Ti4 = fecha.getTime();
-            } else {
-                this.matricula5 = mat;
-                this.Ti5 = fecha.getTime();
+            if (this.matricula == null){
+                Garaxe.numeroCoches = numeroCoches++;
+                this.matricula = mat;
+                this.ti = System.currentTimeMillis();               
+            } else if(this.matricula !=null){
+                JOptionPane.showMessageDialog(null, "Plaza xa ocupada. Inténteo de novo ou máis tarde. Disculpe as molestias.");
             }
-            this.numeroCoches = numeroCoches++;
-            
+           
         }
 
     }
-
-    public public double calcularPrezo() {
-    
+    public void obterTf(){
+        this.tf = System.currentTimeMillis();
+    }
+    public void calcularPrezo() {
+        this.tiempo= this.getTf()-this.getTi();       
+        double calculo = (this.tiempo /1000)-3 + Garaxe.PREZOINICIAL * 0.20;
+        JOptionPane.showMessageDialog(null, "O prezo é de "+calculo+"€.");
+        this.prezo = calculo;
+    }
+    public void pagar(){        
+        
+        double p = Double.parseDouble(JOptionPane.showInputDialog(null, "Introduce a cantidade(€), que vai introducir para pagar."));
+        
+            if (this.prezo>p)
+        JOptionPane.showMessageDialog(null, "Crédito insuficente.");
+            else{
+                this.prezodev = this.prezo - p;
+                this.prezorec = p;
+            }
+                        
+    }
+    public void devolverNulos(){
+        this.matricula=null;
+        Garaxe.numeroCoches=numeroCoches--;
+    }
+    public void factura(){
+       JOptionPane.showMessageDialog(null, "****FACTURA****\n 1. MATRÍCULA: "+this.matricula+""
+               + "\n2. TEMPO: "+this.tiempo+"\n3. PERZO: "+this.prezo+"\n4. CARTOS RECIBIDOS: "+this.prezorec+"\5."
+               + " CARTOS DEVOLTOS: "+this.prezodev+"\n\nGRAZAS POR EMPLEAR O NOSO APARCADOIRO.");
     }
 }
